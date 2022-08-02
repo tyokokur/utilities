7cf5027473094c9af271e36bae1a5a58405e5a68
tyokokur <takashi.yokokura@berkeley.edu>
2022-08-02 15:08:17 -0700

    autocommit

--- colab/tjy_udf.py
+++ colab/tjy_udf.py
@@ -17,8 +17,7 @@ def lighten_color(color, amount=0.5):
     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
     return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
 
-def H_find(filename, b0):
-  thresh = 1e-04
+def H_find(filename, b0, thresh):
 
   df = pd.read_csv(filename, sep="\s+", skiprows=0)
   Nx = len(df.index)
