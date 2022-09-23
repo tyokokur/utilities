f1548b88e95a8efa89479b7c08086f649a6bd697
tyokokur <takashi.yokokura@berkeley.edu>
2022-09-23 12:05:04 -0700

    autocommit

--- colab/tjy_udf.py
+++ colab/tjy_udf.py
@@ -81,3 +81,119 @@ def Kap_D(Cs):
   lb = e**2 * beta / (4*pi * ep0 * epr)
   kappa = sqrt(4*pi * lb * (1*Cs + 1*Cs))
   return kappa
+
+def plot_anim(fname, lx=100, dx=17/150, b0=1.0, html_render='jshtml'):
+    from matplotlib import animation, rc
+    import numpy as np, pandas as pd
+
+    lx = 350
+    dx = 17/150
+    if lx == 100 or lx == 300 or lx == 275 or lx == 350: nx = int(np.floor(lx / dx))
+    else: nx = int(np.ceil(lx/dx))
+
+    ## Unknown bug: 
+    # for lx = 100, nx = np.floor, z = np.arange(...(lx-dx)/b0)
+    # for lx = 125, 150, nx = np.ceil,  z = np.arange(...(lx)/b0)
+
+    ## Iteration sim:
+    itname = fname
+    # itname = 'it_a030c010_x115s002.dat'
+    df = pd.read_csv(itname, sep="\s+", skiprows=0, header=None)
+    ## len it does not match len SIM.dat
+    print("it len ", np.size(df,0))
+
+    phname = 'SIM.dat' 
+    ph = pd.read_csv(phname, sep="\s+", skiprows=0, header=None).to_numpy()
+    nframes = np.min([int(len(df)), int(len(ph)/nx)])
+    print("nframes calc ", nframes)
+    phn = np.reshape(ph, (int(len(ph)/nx), nx))
+    print("phn len ", np.size(phn, 0))
+
+    %matplotlib inline
+
+    # create a figure and axes
+    fig = plt.figure(figsize=(15, 9))
+    ax1 = plt.subplot(1,2,1)
+    ax2 = plt.subplot(2,4,3)
+    ax3 = plt.subplot(2,4,4)
+    ax4 = plt.subplot(2,2,4)
+    plt.subplots_adjust(hspace=0.40)
+    plt.subplots_adjust(wspace=0.35)
+
+    # set up the subplots as needed
+    ax1.set_xlim(( 0, lx))            
+    ax1.set_ylim((0, 1.0))
+    ax1.set_xlabel('z/b')
+    ax1.set_ylabel(r'$\langle\phi_p(z)\rangle_{xy}$')
+                
+    ax3.set_ylim((1e-6, 1))
+    ax3.set_xlim((0, 100))
+    ax3.set_xlabel('it')
+    ax3.set_title('inCompMax')
+    ax3.set_yscale('log')
+    ax3.plot([0,5e5], np.ones(2)*2e-6, ':r')
+                
+    ax2.set_ylim((1e-10, 1))
+    ax2.set_xlim((0, 100))
+    ax2.set_xlabel('it')
+    ax2.set_title('freeDiff')
+    ax2.set_yscale('log')
+    ax2.plot([0,5e5], np.ones(2)*1e-9, ':r')
+
+    ax4.set_ylim((5e-4, 1))
+    ax4.set_xlim((0, 100))
+    ax4.set_xlabel('it')
+    ax4.set_title('andErr')
+    ax4.set_yscale('log')
+    ax4.plot([0,5e5], np.ones(2)*1e-3, ':r')
+
+    # create objects that will change in the animation. These are
+    # initially empty, and will be given new values for each frame
+    # in the animation.
+    txt_title = ax1.set_title('')
+    line1, = ax1.plot([], [], 'k')     # ax.plot returns a list of 2D line objects
+    line2, = ax2.plot([], [], 'r')
+    line3, = ax3.plot([], [], 'b')
+    line4, = ax4.plot([], [], 'g')
+
+    ## Comment to preview subplots
+    plt.close()
+
+    # animation function. This is called sequentially
+    def drawframe(n):
+        ## b0 global from function input 
+        nskip = nframes/100
+        n = round(n * nskip)
+        if n > nframes - 1: n = nframes-1
+        if lx == 100 or lx ==300 or lx==275 or lx == 350: z = np.arange(0, (lx-dx)/b0, dx)
+        else: z = np.arange(0, (lx)/b0, dx)
+        pha = phn[n,:]
+
+        x  = df.iloc[:n,0]
+        y1 = df.iloc[:n,2]
+        y2 = df.iloc[:n,3]
+        y3 = df.iloc[:n,4]
+        line1.set_data(z, pha)
+        line2.set_data(x, y1)
+        line3.set_data(x, y2)
+        line4.set_data(x, y3)
+
+        ax1.set_ylim(0, max(pha)*1.2)
+        if not x.empty: 
+            txt_title.set_text('it = {0:4d}'.format(x[n-1]))
+            ax2.set_xlim(0, max(x)*1.1)
+            ax3.set_xlim(0, max(x)*1.1)
+            ax4.set_xlim(0, max(x)*1.1)
+        if not y1.empty: ax3.set_ylim(1e-6,  max(y2)*1.5)
+        if not y2.empty: ax2.set_ylim(1e-10, max(y1)*1.5)
+        if not y3.empty: ax4.set_ylim(5e-4,  max(y3)*1.5)
+        return (line1,line2) 
+
+    if np.abs(nframes-np.size(phn, 0))<=2:
+        anim = animation.FuncAnimation(fig, drawframe, frames=100, interval=100, blit=True)
+        rc('animation', html=html_render)
+        anim
+    else: 
+        print("nframes, phn mismatch > 2")    
+    
+    return 
