94c34523cac986dfb2cfd7f8c5f952765ffe1567
tyokokur <takashi.yokokura@berkeley.edu>
2022-10-04 13:00:09 -0700

    autocommit

--- python/tjy_udf.py
+++ python/tjy_udf.py
@@ -79,20 +79,32 @@ def plot_pha(read_list, labs=[], b0=1.0,
       # Plot total density (zorder 3)
       rshi.iloc[1:,0] = phAr.iloc[1:,0] # Change here if shift wanted; can change udf inputs if needed
       lshi.iloc[1:,0] = phAr.iloc[1:,0]
-      axs[0].plot(np.multiply(rshi.iloc[1:,0], div[k]), phAr.iloc[1:,1], color = colors[k], zorder = 3, alpha=1.0)
-      axs[1].plot(np.multiply(lshi.iloc[1:,0], div[k]), phAr.iloc[1:,1], color = colors[k], zorder = 3, alpha=1.0, label=labs[k])
+      if double: 
+          axs[0].plot(np.multiply(rshi.iloc[1:,0], div[k]), phAr.iloc[1:,1], color = colors[k], zorder = 3, alpha=1.0)
+          axs[1].plot(np.multiply(lshi.iloc[1:,0], div[k]), phAr.iloc[1:,1], color = colors[k], zorder = 3, alpha=1.0, label=labs[k])
+      else: 
+          ax.plot(np.multiply(rshi.iloc[1:,0], div[k]), phAr.iloc[1:,1], color = colors[k], zorder = 3, alpha=1.0)
 
       # Plot chain types (zorder 2)
       step = 1
-      for j in range(len(block_Nik)):
-        axs[0].plot(np.multiply(phAT.iloc[1:,0], div[k]), phAT.iloc[1:,j+1], '-',  zorder=2, color=lighten_color(colors_i[k], amount=0.50),label='_Total')
+      if double:
+          for j in range(len(block_Nik)):
+            axs[0].plot(np.multiply(phAT.iloc[1:,0], div[k]), phAT.iloc[1:,j+1], '-',  zorder=2, color=lighten_color(colors_i[k], amount=0.50),label='_Total')
 
-        #Plot block densities (zorder 3)
-        for i in range(block_Nik[j]):
-          if (i == block1): axs[0].plot(np.multiply(ph1r.iloc[1:,0], div[k]), ph1r.iloc[1:,i+step], '--',  zorder=3, color=lighten_color(colors_i[k], amount=0.60),label='_Block')
-          if (i == block2): axs[1].plot(np.multiply(ph1r.iloc[1:,0], div[k]), np.multiply(ph1r.iloc[1:,i+step],1), '--',  zorder=3, color=lighten_color(colors_i[k], amount=0.60),label='_Block')
+            #Plot block densities (zorder 3)
+            for i in range(block_Nik[j]):
+              if (i == block1): axs[0].plot(np.multiply(ph1r.iloc[1:,0], div[k]), ph1r.iloc[1:,i+step], '--',  zorder=3, color=lighten_color(colors_i[k], amount=0.60),label='_Block')
+              if (i == block2): axs[1].plot(np.multiply(ph1r.iloc[1:,0], div[k]), np.multiply(ph1r.iloc[1:,i+step],1), '--',  zorder=3, color=lighten_color(colors_i[k], amount=0.60),label='_Block')
+            step += block_Nik[j]
+      else: 
+          for j in range(len(block_Nik)):
+            ax.plot(np.multiply(phAT.iloc[1:,0], div[k]), phAT.iloc[1:,j+1], '-',  zorder=2, color=lighten_color(colors_i[k], amount=0.50),label='_Total')
 
-        step += block_Nik[j]
+            #Plot block densities (zorder 3)
+            for i in range(block_Nik[j]):
+              if (i == block1): ax.plot(np.multiply(ph1r.iloc[1:,0], div[k]), ph1r.iloc[1:,i+step], '--',  zorder=3, color=lighten_color(colors_i[k], amount=0.60),label='_Block')
+
+            step += block_Nik[j]
 
     conf = [Patch(facecolor=i, edgecolor='k', lw=1.5) for i in colors_i]
     conf += [
