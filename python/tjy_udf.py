d92ca84716303a6cb5c3e1c01ad715c63eb88111
tyokokur <110191996+tyokokur@users.noreply.github.com>
2022-10-03 22:05:46 -0700

    autocommit

--- python/tjy_udf.py
+++ python/tjy_udf.py
@@ -1,4 +1,4 @@
-def plot_pha(read_list, labs=[], b0=1.0, y1_start=0, y1_end=1.00, y2_start=0, y2_end=1.00, x1_start=0, x1_end=100, x2_start=0, x2_end=100): 
+def plot_pha(read_list, labs=[], b0=1.0, y1_start=0, y1_end=1.00, x1_start=0, x1_end=100, y2_start=None, y2_end=None,  x2_start=None, x2_end=None): 
     from matplotlib import animation, rc
     import numpy as np, pandas as pd, matplotlib.pyplot as plt
     from matplotlib.lines import Line2D
@@ -6,6 +6,7 @@ def plot_pha(read_list, labs=[], b0=1.0, y1_start=0, y1_end=1.00, y2_start=0, y2
     from matplotlib import ticker
    
     if not labs: labs = read_list #If labs is empty, default is read_list
+    if not x1_start: x1_start = x1_start
     
     div = [b0]*len(read_list) #b0
     block_Num = len(read_list)
