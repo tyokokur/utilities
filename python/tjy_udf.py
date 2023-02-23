7093146be4678795a435d9c804e0558f80fe5b0c
tyokokur <110191996+tyokokur@users.noreply.github.com>
2023-02-22 23:29:49 -0800

    autocommit

--- python/tjy_udf.py
+++ python/tjy_udf.py
@@ -257,7 +257,8 @@ def plot_pha_feed(read_file, ax, labs=[], b0=1.0, show=True,
     for j in range(len(block_Nik)):
     #Plot block densities (zorder 3)
         for i in range(block_Nik[j]):
-            if (i == block1): ax.plot(lshi[0], ph1r.iloc[:,i+step], '--',  zorder=3, color=lighten_color(color, amount=0.60),label='_Block')
+            if not lightf: if (i == block1): ax.plot(lshi[0], ph1r.iloc[:,i+step], '--',  zorder=3, color=lighten_color(color, amount=0.60),label='_Block')
+            else: if (i == block1): ax.plot(lshi[0], ph1r.iloc[:,i+step], '--',  zorder=3, color=lighten_color(color, amount=lightf),label='_Block')
 
         step += block_Nik[j]
 
