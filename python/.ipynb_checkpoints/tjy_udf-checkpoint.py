2318af4d0e1ea06acff72424fb97e3aac216aabf
tyokokur <takashi.yokokura@berkeley.edu>
2023-02-24 14:41:23 -0800

    autocommit

--- python/.ipynb_checkpoints/tjy_udf-checkpoint.py
+++ python/.ipynb_checkpoints/tjy_udf-checkpoint.py
@@ -259,9 +259,9 @@ def plot_pha_feed(read_file, ax, labs=[], b0=1.0, show=True,
     #Plot block densities (zorder 3)
         for i in range(block_Nik[j]):
             if not y1_shift: 
-                if (i == block1): ax.plot(lshi[0], ph1r.iloc[:,i+step] + y1_shift, '--',  zorder=3, color=lighten_color(color, amount=lightf),label='_Block')
-            else: 
                 if (i == block1): ax.plot(lshi[0], ph1r.iloc[:,i+step], '--',  zorder=3, color=lighten_color(color, amount=lightf),label='_Block')
+            else: 
+                if (i == block1): ax.plot(lshi[0], ph1r.iloc[:,i+step]+y1_shift, '--',  zorder=3, color=lighten_color(color, amount=lightf),label='_Block')
 
         step += block_Nik[j]
 
