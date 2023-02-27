5fe310a001e4f8c079cf0adc18d026f3b3d0e926
tyokokur <takashi.yokokura@berkeley.edu>
2023-02-27 14:03:14 -0800

    autocommit

--- python/tjy_udf.py
+++ python/tjy_udf.py
@@ -189,6 +189,7 @@ def plot_pha_feed(read_file, ax, labs=[], b0=1.0, show=True,
     # Defaults
     if not labs:     labs     = read_file
     if not x1_shift: x1_shift = 0
+    if not y1_shift: y1_shift = 0
     if not block_Ni: block_Ni = [5]
 
     block_Nik = block_Ni
@@ -244,10 +245,7 @@ def plot_pha_feed(read_file, ax, labs=[], b0=1.0, show=True,
 
     # Plot total density (zorder 3)
     lshi[0] = phAr[0] + x1_shift # Shifted phA for axs[0]
-    if not y1_shift: 
-        ushi[0] = phAr.iloc[:,1] # Shifted phA for axs[0]
-    else: 
-        ushi[0] = phAr.iloc[:,1] + y1_shift
+    ushi[0] = phAr.iloc[:,1] + y1_shift
 
     if not lightf: ax.plot(lshi[0], ushi[0], color = color, zorder = 2, alpha=1.0, label=labs[0])
     else:          ax.plot(lshi[0], ushi[0], color = lighten_color(color, amount=lightf), zorder = 2, alpha=1.0, label=labs[0])
