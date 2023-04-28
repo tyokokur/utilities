a07e5d2b560eeddeb472fcb53f0a7b768b2f2506
tyokokur <110191996+tyokokur@users.noreply.github.com>
2023-04-28 15:10:15 -0700

    autocommit

--- python/NFH_train.py
+++ python/NFH_train.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 GIT = 'https://github.com/tyokokur/tmpdat/raw/main/nfh/'
-dat = pd.read_csv(GIT+'NFH_train.txt', sep='\s+')
+dat = pd.read_csv(GIT+'NFH_train.txt', sep='\s+', names=['parent','chiname','b','v',
+                                                         'cs','gH','tH0','tH1','tH2','tH3'])
