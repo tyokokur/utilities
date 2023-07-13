46da17ef5ef8739e1bda0e96a7743c44041a4629
tyokokur <110191996+tyokokur@users.noreply.github.com>
2023-07-12 17:06:23 -0700

    autocommit

---
+++ python/untitled.py
@@ -0,0 +1,5 @@
+import pandas as pd, numpy as np
+def conv_dat(AA): return pd.DataFrame(np.array(AA).reshape(4,4), columns=['cs', 'h1', 'h2', 'h3'])
+
+## FILE: purebrushdata.xlxs (Erika sent 7/12/23)
+LSA = [ 1.6, 
