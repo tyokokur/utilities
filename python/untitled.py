7a900f20f0859770cdb1ede46aa3a412733a8946
tyokokur <110191996+tyokokur@users.noreply.github.com>
2023-07-12 17:08:23 -0700

    autocommit

--- python/untitled.py
+++ python/untitled.py
@@ -2,4 +2,15 @@ import pandas as pd, numpy as np
 def conv_dat(AA): return pd.DataFrame(np.array(AA).reshape(4,4), columns=['cs', 'h1', 'h2', 'h3'])
 
 ## FILE: purebrushdata.xlxs (Erika sent 7/12/23)
-LSA = [ 1.6, 
+LSA = [ 1.6, 43.27421,	49.69911,	33.97335,	
+        4.6, 
+
+38.36728,	30.35,	35.64,		
+40.96,	31.02,	60.50798,
+55.45439,	43.32828,	42.18774
+       
+       
+       
+       
+       
+       
