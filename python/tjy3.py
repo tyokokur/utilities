6c503c0507c8331c7fa59e2a5e01ad24444fa6c9
tyokokur <110191996+tyokokur@users.noreply.github.com>
2025-02-09 16:38:34 -0800

    autocommit

--- python/tjy3.py
+++ python/tjy3.py
@@ -126,8 +126,9 @@ class Pha3D:
                         if 'e' in reflect_over: I = 2*nx-1 - i
                         else: 
                             I = nx-1 - i
-                            i = i + nx
+                            i = i + nx-1
                         if 'n' in reflect_over: J = 2*ny-1 - j
+                        else: J = 2*ny-1 - j ##TEMPORARY
                         PH2[_conv2(i,j,k)] = vol[_conv1(i,j,k)]
                         PH2[_conv2(I,j,k)] = vol[_conv1(i,j,k)]
                         PH2[_conv2(i,J,k)] = vol[_conv1(i,j,k)]
