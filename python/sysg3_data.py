dd1c5f1e616cbf447785330c6b3605ad71ac5152
tyokokur <takashi.yokokura@berkeley.edu>
2025-02-24 13:58:49 -0800

    autocommit

--- python/sysg3_data.py
+++ python/sysg3_data.py
@@ -10,7 +10,7 @@ class All:
     def show(self): 
         styler = self.all.iloc[:,:4] \
             .style.hide(axis='index') \
-            .format(precision=3, type='G')
+            .format(precision=3, formatter='{:E}')
         display(styler)
         
     def find(self, condition, n_cond=1):
