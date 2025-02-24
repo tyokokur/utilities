e4d87caa86350f63e82f71bf0f598653aef7737e
tyokokur <takashi.yokokura@berkeley.edu>
2025-02-24 13:22:09 -0800

    autocommit

--- python/sysg3_data.py
+++ python/sysg3_data.py
@@ -1,8 +1,22 @@
 from Data import Data
 ''' chi = 1.25, N = 200, b = 1.0, v = 4.19, T = 293 '''
 
+class all:
+    def __init__(self):
+        sdlfkj
+        
+    def display(self):
+        print(self)
+        
 ''' alpha = 0.00, sigma = 5e-03 '''
 a0s5_hom = Data(0.00, 5e-03, 'hom')
 a0s5_hom.new((8, 13.8, 15) , -2.98579, icm=5.5e-06, err=4.0e-04)
 a0s5_hom.new((13.8,13.8,15), -2.98578, icm=2.2e-06, err=1.3e-04)
 a0s5_hom.new((10.6,10.6,15), -2.98580, icm=1.0e-05, err=3.7e-05)
+
+a0s5_hol = Data(0.00, 5e-03, 'hol')
+a0s5_hol.new((8, 13.8, 15)   , -2.98419, icm=3.2e-06, err=4.0e-04)
+a0s5_hol.new((13.8, 13.8, 15), -2.98544, icm=1.1e-05, err=4.5e-04)
+a0s5_hol.new((15, 15,15)     , -2.98573, icm=2.1e-04, err=6.7e-04)
+a0s5_hol.new((16, 16,15)     , -2.98602, icm=5.5e-05, err=1.8e-04)
+a0s5_hol.new((17, 17,15)     , -2.98620, icm=6.8e-05, err=2.0e-04)
