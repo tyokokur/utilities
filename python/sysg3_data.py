5434ffa015537d9384cdc1a9eb3f43e49d67c5b4
tyokokur <takashi.yokokura@berkeley.edu>
2025-04-09 13:13:05 -0700

    autocommit

--- python/sysg3_data.py
+++ python/sysg3_data.py
@@ -370,6 +370,10 @@ datasets += pack_data('a25s3_mul', a25s3_mul)
 ''' alpha = 0.30, sigma = 0.020 '''
 a3s2_cyl = Data(0.30, 0.020, 'cyl')
 a3s2_cyl.new((15.2, 17.6, 64.2), -10.4873, icm=3.6e-02, err=7.4e-03)
+a3s2_cyl.new((16.2, 17.6, 64.2), -10.4922, icm=3.4e-02, err=6.8e-03)
+a3s2_cyl.new((15.2, 16.2, 64.2), -10.4931, icm=2.3e-03, err=4.7e-03)
+a3s2_cyl.new((14.0, 16.2, 64.2), -10.4922, icm=2.1e-02, err=4.6e-03)
+a3s2_cyl.new((16.2, 18.2, 64.2), -10.4923, icm=2.5e-02, err=5.0e-03)
 a3s2_cyl.multi = True
 datasets += pack_data('a3s2_cyl', a3s2_cyl)
 
