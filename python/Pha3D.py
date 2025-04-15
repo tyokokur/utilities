f4e997a6b8977bbd1fdd43d353fbbea296abec94
tyokokur <takashi.yokokura@berkeley.edu>
2025-04-15 12:49:33 -0700

    autocommit

--- python/Pha3D.py
+++ python/Pha3D.py
@@ -396,13 +396,13 @@ class Pha3D:
             walls = []
             xmin, xmax = xs[0], xs[1]
             ymin, ymax = ys[0], ys[1]
-            s, t = np.meshgrid(np.linspace(xmin, xmax, 10), np.linspace(ymin, ymax, 10))
+            s, t = np.meshgrid(np.linspace(xmin, xmax, 1000), np.linspace(ymin, ymax, 1000))
             walls.append(go.Surface(x=s,y=t,z=z_max*np.ones(s.shape), colorscale=lin_cscale(wall_col), showscale=False))
             walls.append(go.Surface(x=s,y=t,z=np.zeros(s.shape), colorscale=lin_cscale(floor_col), showscale=False))
-            u, v = np.meshgrid(np.linspace(xmin, xmax, 10), np.linspace(0, z_max, 10))
+            u, v = np.meshgrid(np.linspace(xmin, xmax, 1000), np.linspace(0, z_max, 1000))
             walls.append(go.Surface(x=u,y=ymax*np.ones(u.shape),z=v, colorscale=lin_cscale(wall_col), showscale=False))
             walls.append(go.Surface(x=u,y=ymin*np.ones(u.shape),z=v, colorscale=lin_cscale(wall_col), showscale=False))
-            u, v = np.meshgrid(np.linspace(ymin, ymax, 10), np.linspace(0, z_max, 10))
+            u, v = np.meshgrid(np.linspace(ymin, ymax, 1000), np.linspace(0, z_max, 1000))
             walls.append(go.Surface(x=xmax*np.ones(u.shape),y=u,z=v, colorscale=lin_cscale(wall_col), showscale=False))
             walls.append(go.Surface(x=xmin*np.ones(u.shape),y=u,z=v, colorscale=lin_cscale(wall_col), showscale=False))
             return walls
