650755a6ce526493a8cd7e9bc130faeb6932090a
tyokokur <110191996+tyokokur@users.noreply.github.com>
2025-05-01 10:21:13 -0700

    autocommit

--- python/sysg3_data.py
+++ python/sysg3_data.py
@@ -838,8 +838,10 @@ a3s4_cyl.new((14.6, 14.6, 86.6), -20.5559, icm=5.6e-03, err=8.0e-04)
 datasets += pack_data('a3s4_cyl', a3s4_cyl)
 
 a3s4_pn  = Data(0.30, 0.040, 'str')
+a3s4_pn.new((10.2, 20.2, 86.6), -20.5585, icm=7.6e-03, err=9.6e-04)
+a3s4_pn.new((12.2, 18.2, 86.6), -20.5584, icm=7.6e-03, err=8.6e-04)
 a3s4_pn.new((12.2, 20.2, 86.6), -20.5581, icm=1.0e-02, err=9.0e-04)
-a3s4_pn.new((14.6, 20.2, 86.6), -20.5581, icm=1.0e-02, err=9.0e-04)
+a3s4_pn.new((14.6, 20.2, 86.6), -20.5561, icm=1.1e-02, err=1.5e-03)
 datasets += pack_data('a3s4_pn', a3s4_pn)
 
 a3s4_str = Data(0.30, 0.040, 'str')
