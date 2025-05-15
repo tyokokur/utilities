3166dd30020a2437ae5d447dcec8fae93eae4da9
tyokokur <takashi.yokokura@berkeley.edu>
2025-05-15 10:21:36 -0700

    autocommit

--- python/sysg3_data.py
+++ python/sysg3_data.py
@@ -315,7 +315,9 @@ datasets += pack_data('a1s2_hom', a1s2_hom)
 ''' alpha = 0.15, sigma = 0.005 '''
 a15s05_mic = Data(0.15, 0.005, 'mic')
 a15s05_mic.new((10.2, 10.2, 54.2), -2.88236, icm=2.9e-02, err=3.3e-03)
-# a15s05_mic.new((13.2, 13.2, 54.2), -2.88507, icm=1.3e-02, err=1.1e-03) # probably hole
+a15s05_mic.new((13.2, 13.2, 54.2), -2.88507, icm=1.3e-02, err=1.1e-03) # hole
+a15s05_mic.new((14.2, 14.2, 54.2), -2.88543, icm=7.7e-03, err=8.1e-04) # hole
+a15s05_mic.new((15.2, 15.2, 54.2), -2.88569, icm=5.1e-03, err=7.2e-04) # hole
 datasets += pack_data('a15s05_mic', a15s05_mic)
 
 a15s05_str = Data(0.15, 0.005, 'str')
