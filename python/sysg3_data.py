6c5f7632420585804e982f2c79f004c97ef49910
tyokokur <takashi.yokokura@berkeley.edu>
2025-05-15 10:30:55 -0700

    autocommit

--- python/sysg3_data.py
+++ python/sysg3_data.py
@@ -341,7 +341,7 @@ a15s1_mic.new((12.2, 12.2, 54.2), -5.78556, icm=5.2e-03, err=6.4e-04) # Need to 
 datasets += pack_data('a15s1_mic', a15s1_mic)
 
 a15s1_str= Data(0.15, 0.010, 'str') #melted
-a15s1_str.new((16.2, 20.2, 54.2), -5.78588, icm=8.3e-03, err=1.0e-03) # single
+a15s1_str.new((16.2, 20.2, 54.2), -5.78588, icm=3.7e-03, err=5.6e-04) # single, starting to flatten
 a15s1_str.new((17.2, 20.2, 54.2), -5.78577, icm=8.3e-03, err=1.0e-03) # single
 # a15s1_str.new((20.2, 15.2, 54.2), -5.78487, icm=2.0e-02, err=2.6e-03) # double 
 a15s1_str.new((20.2, 16.2, 54.2), -5.78568, icm=7.7e-03, err=1.0e-03) # single
