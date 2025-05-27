756e66735fb93ae11e0a3083581af058f4423db2
tyokokur <110191996+tyokokur@users.noreply.github.com>
2025-05-26 20:37:40 -0700

    autocommit

--- python/Census.py
+++ python/Census.py
@@ -20,11 +20,7 @@ class Census:
         self.q_list   = df.columns.to_list()
         
         # Complete
-        if datarange == (0, 0): 
-            lastcol = df.shape[1]+self.orig_datarange[0]+datarange[0]-1
-        else: 
-            lastcol = self.orig_datarange[1]+datarange[1]
-        
+        lastcol = df.shape[1]+self.orig_datarange[0]+datarange[0]-1
         print('Initialization completed.')
         print('Data recorded')
         print('\tfrom column {}: \n\t\t"{}"'.format(num_to_exel_col(datarange[0]), self.q_list[0]))
