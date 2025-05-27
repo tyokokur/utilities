cf7c3c8bc9d489203484a607311661b7bdc50d68
tyokokur <110191996+tyokokur@users.noreply.github.com>
2025-05-26 19:50:17 -0700

    autocommit

---
+++ python/untitled.py
@@ -0,0 +1,9 @@
+import numpy as np, pandas as pd
+import matplotlib.pyplot as plt, matplotlib as mpl
+
+class Census:
+    """
+    docstring for Census class
+    """
+    def __init__(self): 
+        self.questions = 
