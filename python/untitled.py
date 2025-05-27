c55c9649af8c6e865adc7a81eae5a78481dc5ca9
tyokokur <110191996+tyokokur@users.noreply.github.com>
2025-05-26 19:54:18 -0700

    autocommit

--- python/untitled.py
+++ python/untitled.py
@@ -5,7 +5,21 @@ class Census:
     """
     docstring for Census class
     """
-    def __init__(self, filename): 
+    def __init__(self, filepath, header=1, datarange=(17,None)): 
+        from pathlib import Path
+        p = Path(filepath)
+        df = pd.read_csv(p, header=header)
         
+        # Discard IP addresses
+        df = df.drop([0])
+
+        # No previews
+        df = df[df['Response Type']!='Survey Preview'].reset_index(drop=True)
+
+        # Discard unnecessary data
+        df = df.iloc[:,17:]
+
+        # Print data summary
+        print('Data read into df1.\n{} responses.\n{} questions asked.'.format(*df1.shape))
         
     
