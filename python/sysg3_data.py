c098cd40934048894011de3488535b17fa85307a
tyokokur <110191996+tyokokur@users.noreply.github.com>
2025-06-28 10:18:48 -0700

    autocommit

--- python/sysg3_data.py
+++ python/sysg3_data.py
@@ -477,7 +477,18 @@ datasets += pack_data('a1s5_hom', a1s5_hom)
 a1s625_str = Data(0.10, 6.25e-03, 'str')
 a1s625_str.new((19.0, 20.2, 40.2), -3.67979, icm=1.3e-03, err=2.1e-04)
 a1s625_str.new((21.8, 20.2, 40.2), -3.67994, icm=5.9e-03, err=1.0e-03)
+a1s625_str.new((24.2, 20.2, 40.2), -3.68011, icm=4.8e-03, err=9.4e-04)
 datasets += pack_data('a1s625_str', a1s625_str)
+
+a1s675_hol = Data(0.10, 6.25e-03, 'hol')
+a1s675_hol.new((27.2, 32.2, 36.2), -3.68109, icm=1.8e-02, err=8.8e-04)
+a1s675_hol.new((30.2, 32.2, 36.2), -3.68117, icm=1.8e-02, err=9.7e-04)
+datasets += pack_data('a1s675_hol', a1s675_hol)
+
+a1s675_hol = Data(0.10, 6.25e-03, 'hol')
+a1s675_hol.new((27.2, 32.2, 36.2), -3.68109, icm=1.8e-02, err=8.8e-04)
+a1s675_hol.new((30.2, 32.2, 36.2), -3.68117, icm=1.8e-02, err=9.7e-04)
+datasets += pack_data('a1s675_hol', a1s675_hol)
 
 a1s675_hom = Data(0.10, 6.25e-03, 'hom')
 a1s675_hom.new((20.2, 20.2, 40.2), -3.68177, icm=2.0e-05, err=2.2e-06)
