f720bde60b730284364c8db8b08b3d769c978125
tyokokur <takashi.yokokura@berkeley.edu>
2025-07-28 13:59:58 -0700

    autocommit

--- python/untitled.py
+++ python/untitled.py
@@ -1,9 +1,9 @@
-import numpy as np, pandas as pd, matplotlib.pyplot as plt, matplotlib as mpl
+import numpy as np, pandas as pd, matplotlib.pyplot as plt, matplotlib as mpl, plotly.graph_objects as go
 from Pha3D import Pha3D
 
 class Process3D:
-    import plotly.graph_objects as go
 
-pha3d = Pha3D(f2_fnames[1], dims=f2_boxes[1], fprefix=fp)
-data = pha3d.get_vol(isomin=0.10, fprefix=fp, 
-                     n_coarse=1, zmax=25.0, reflect_over='sw', write_html=False)
+    def _get_data(self)
+    pha3d = Pha3D(f2_fnames[1], dims=f2_boxes[1], fprefix=fp)
+    data = pha3d.get_vol(isomin=0.10, fprefix=fp, 
+                         n_coarse=1, zmax=25.0, reflect_over='sw', write_html=False)
