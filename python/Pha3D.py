c527a040d0d8e7bfe75e940f77985453b23a194b
tyokokur <takashi.yokokura@berkeley.edu>
2025-07-31 15:47:10 -0700

    autocommit

--- python/Pha3D.py
+++ python/Pha3D.py
@@ -366,8 +366,8 @@ class Pha3D:
         ## new 
 
         xx = plt.contourf(XX, ZZ, PHA_Y, **kws)
-        # for collection in xx.collections:
-        #     collection.set_edgecolor('face')
+        for collection in xx.collections:
+            collection.set_edgecolor('face')
         FIL = [xx]
         
         # old: 
